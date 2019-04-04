c5359e7db09e019598cac3424e94b0b8314f23d7
camila <camidlserna@hotmail.com>
2019-04-04 19:15:47 -0300

    how it works 'usecases'

---
+++ src/app/sections/how-it-works/loan-life/loan-life.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loan-life',
+  templateUrl: './loan-life.component.html',
+  styleUrls: ['./loan-life.component.scss']
+})
+export class LoanLifeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
