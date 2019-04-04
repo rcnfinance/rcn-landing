988c28b8dbeaed0c6349ac4e045434353ac57000
camila <camidlserna@hotmail.com>
2019-04-04 15:29:24 -0300

    creditors loan properties

--- src/app/sections/creditors/loan-properties/loan-properties.component.ts
+++ src/app/sections/creditors/loan-properties/loan-properties.component.ts
@@ -7,6 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoanPropertiesComponent implements OnInit {
 
+
+  content = [
+    {icon: "fas fa-user", 
+    title: "BORROWER",
+    data: "ox2c...ef20",
+    icon2: "fas fa-chart-line", 
+    title2: "LENDING AMOUNT",
+    data2: "2500"},
+    {icon: "fas fa-user", 
+    title: "BORROWER",
+    data: "ox2c...ef20",
+    icon2: "fas fa-chart-line", 
+    title2: "LENDING AMOUNT",
+    data2: "2500"},
+    {icon: "fas fa-user", 
+    title: "BORROWER",
+    data: "ox2c...ef20",
+    icon2: "fas fa-chart-line", 
+    title2: "LENDING AMOUNT",
+    data2: "2500"},
+    {icon: "fas fa-user", 
+    title: "BORROWER",
+    data: "ox2c...ef20",
+    icon2: "fas fa-chart-line", 
+    title2: "LENDING AMOUNT",
+    data2: "2500"},
+    {icon: "fas fa-user", 
+    title: "BORROWER",
+    data: "ox2c...ef20",
+    icon2: "fas fa-chart-line", 
+    title2: "LENDING AMOUNT",
+    data2: "2500"},
+    {icon: "fas fa-user", 
+    title: "BORROWER",
+    data: "ox2c...ef20",
+    icon2: "", 
+    title2: "",
+    data2: ""},
+   
+    ];
+
   constructor() { }
 
   ngOnInit() {
