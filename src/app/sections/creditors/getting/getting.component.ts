e2a9d66b5d0f6ae286d3a61b18ab685c8745eed9
camila <camidlserna@hotmail.com>
2019-04-04 17:37:36 -0300

    creditors 'getting started'

--- src/app/sections/creditors/getting/getting.component.ts
+++ src/app/sections/creditors/getting/getting.component.ts
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GettingComponent implements OnInit {
 
+  content = [
+    {icon: "../../../../assets/icons/icon-borderless.png", 
+    title: "GET CRYPTO",
+    data: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."},
+    {icon: "../../../../assets/icons/icon-borderless.png", 
+    title: "GET CRYPTO",
+    data: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."},
+    {icon: "../../../../assets/icons/icon-borderless.png", 
+    title: "GET CRYPTO",
+    data: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."},
+    ];
+
   constructor() { }
 
   ngOnInit() {
