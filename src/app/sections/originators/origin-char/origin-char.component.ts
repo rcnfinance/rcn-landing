893222b9f13bae82719d0eea0dc8ce4f78dcb758
camila <camidlserna@hotmail.com>
2019-04-04 18:01:41 -0300

    originators header

---
+++ src/app/sections/originators/origin-char/origin-char.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-origin-char',
+  templateUrl: './origin-char.component.html',
+  styleUrls: ['./origin-char.component.scss']
+})
+export class OriginCharComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
