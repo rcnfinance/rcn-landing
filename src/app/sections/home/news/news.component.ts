87f5d41bb8e8bcd1aaa40bc093a8547012088078
camila <camidlserna@hotmail.com>
2019-04-05 10:55:31 -0300

    building home carrousel

--- src/app/sections/home/news/news.component.ts
+++ src/app/sections/home/news/news.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import { DragScrollComponent } from '../../../../../src/ngx-drag-scroll';
 
 @Component({
   selector: 'app-news',
@@ -7,6 +9,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsComponent implements OnInit {
 
+  hideScrollbar;
+  disabled;
+  xDisabled;
+  yDisabled;
+  // imagelist = [
+  //   'icon-borderless.png',
+  //   'icon-borderless.png',
+  //   'icon-borderless.png',
+  //   'icon-borderless.png' ,
+  //   'icon-borderless.png',
+  //   'icon-borderless.png',
+  //   'icon-borderless.png',
+  //   'icon-borderless.png',
+  //   'icon-borderless.png',
+  //   'icon-borderless.png',
+  //   'icon-borderless.png'
+  // ];
+  leftNavDisabled = false;
+  rightNavDisabled = false;
+  index = 0;
+
+    @ViewChild('nav', {read: DragScrollComponent}) ds: DragScrollComponent;
+    
+    moveLeft($event) {
+      this.ds.moveLeft();
+      console.info(123)
+    }
+  
+    moveRight() {
+      this.ds.moveRight();
+    }
+  
+    moveTo(index) {
+      this.ds.moveTo(index);
+    }
+
+    leftBoundStat(reachesLeftBound: boolean) {
+      this.leftNavDisabled = reachesLeftBound;
+    }
+  
+    rightBoundStat(reachesRightBound: boolean) {
+      this.rightNavDisabled = reachesRightBound;
+    }
+  
+    // ngAfterViewInit() {
+    //   // Starting ngx-drag-scroll from specified index(3)
+    //   setTimeout(() => {
+    //     this.ds.moveTo(3);
+    //   }, 0);
+    // }
+  
   
   // @ViewChild('nav', {read: DragScrollComponent}) ds: DragScrollComponent;
   
