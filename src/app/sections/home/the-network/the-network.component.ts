371b55bc196943fdc8a5148028d296f9d74a23b2
Nicolas <menendeznm@gmail.com>
2019-04-08 18:40:31 -0300

    add navigation in home network section

--- src/app/sections/home/the-network/the-network.component.ts
+++ src/app/sections/home/the-network/the-network.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-the-network',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TheNetworkComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
