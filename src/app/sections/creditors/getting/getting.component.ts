bb213d97f90b5b89f5657c725f96386bc13771c9
camila <camidlserna@hotmail.com>
2019-04-09 11:41:14 -0300

    creditors mobile styles

--- src/app/sections/creditors/getting/getting.component.ts
+++ src/app/sections/creditors/getting/getting.component.ts
@@ -8,13 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class GettingComponent implements OnInit {
 
   content = [
-    {icon: "../../../../assets/icons/icon-borderless.png", 
+    {icon: "../../../../assets/icons/1.svg", 
     title: "GET CRYPTO",
     data: "Acquire RCN eligible lending currencies in exchanges, brokers or peer-to-peer platforms."},
-    {icon: "../../../../assets/icons/icon-borderless.png", 
+    {icon: "../../../../assets/icons/2.svg", 
     title: "FUND THE ACCOUNT",
     data: "Download Metamask plug-in for your browser and transfer funds to the wallet with no minimum amount."},
-    {icon: "../../../../assets/icons/icon-borderless.png", 
+    {icon: "../../../../assets/icons/3.svg", 
     title: "START LENDING",
     data: "Access the platform, complete Civic KYC requirements and begin earning passive income."},
     ];
