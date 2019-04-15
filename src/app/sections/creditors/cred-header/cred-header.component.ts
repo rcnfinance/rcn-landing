f860696c7a1633d27a2e0056283cc1d38f92d362
camila <camidlserna@hotmail.com>
2019-04-15 14:19:43 -0300

    adding background numbers and links to 'getting started' on creditors

--- src/app/sections/creditors/cred-header/cred-header.component.ts
+++ src/app/sections/creditors/cred-header/cred-header.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { LandingAnimations } from 'src/app/animations/animations';
 
 @Component({
   selector: 'app-cred-header',
   templateUrl: './cred-header.component.html',
-  styleUrls: ['./cred-header.component.scss']
+  styleUrls: ['./cred-header.component.scss'],
+  animations: LandingAnimations.animations
 })
 export class CredHeaderComponent implements OnInit {
 
