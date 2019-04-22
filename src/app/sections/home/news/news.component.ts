13d794fa012dca5beed27b243396cc2e6c0aad30
camila <camidlserna@hotmail.com>
2019-04-22 10:53:40 -0300

    First and last arrow on gray, at carrousel 'home'

--- src/app/sections/home/news/news.component.ts
+++ src/app/sections/home/news/news.component.ts
@@ -17,6 +17,9 @@ export class NewsComponent implements OnInit {
   activeContent: IContent[] = [];
   activeContentStartIndex: number;
   maxActiveContent = 6;
+  first = true;
+  last = false;
+  screen = 0;
 
   state = 'hide';
   lineState = 'hide';
@@ -170,8 +173,12 @@ export class NewsComponent implements OnInit {
     if (this.activeContentStartIndex > 0) {
       --this.activeContentStartIndex;
       this.activateContent(this.activeContentStartIndex, 'left');
-
     }
+    this.screen --
+    if (this.screen < 0) {
+      this.screen = 0;
+    }
+    this.check()
   }
 
   right() {
@@ -180,7 +187,24 @@ export class NewsComponent implements OnInit {
       ++this.activeContentStartIndex;
       this.activateContent(this.activeContentStartIndex, 'right');
     }
+    this.screen ++
+    if (this.screen > 2) {
+      this.screen = 2;
+    }
+    this.check()
+  }
 
+  check() {
+    if (this.screen == 0) {
+      this.first = true;
+      this.last = false;
+    } else if (this.screen == 2) {
+      this.first = false;
+      this.last = true;
+    } else {
+      this.first = false;
+      this.last = false;
+    }
   }
 }
 
