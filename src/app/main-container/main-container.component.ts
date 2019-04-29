667b31c2a548685fa99cea4c717ccdfd74a781f0
camila <camidlserna@hotmail.com>
2019-04-29 12:29:54 -0300

    Adding animations on menu

--- src/app/main-container/main-container.component.ts
+++ src/app/main-container/main-container.component.ts
@@ -40,6 +40,11 @@ export class MainContainerComponent implements OnInit {
   }
 
   markAsSelected(section: string) {
+    let element = document.getElementById(section);
+    element.classList.add("pulse");
+    setTimeout(function(){ 
+      element.classList.remove("pulse"); 
+    }, 1000);
     this.lineStateCreditors = 'hide';
     this.lineStateOriginators = 'hide';
     this.lineStateDevelopers = 'hide';
@@ -61,7 +66,40 @@ export class MainContainerComponent implements OnInit {
       break;
 
     }
+    
   }
+
+  // addClass1() {
+  // let element = document.getElementById("creditors");
+  // element.classList.add("pulse");
+  // setTimeout(function(){ 
+  //   element.classList.remove("pulse"); 
+  // }, 1000);
+  // }
+
+  // addClass2() {
+  // let element = document.getElementById("originators");
+  // element.classList.add("pulse");
+  // setTimeout(function(){ 
+  //   element.classList.remove("pulse"); 
+  // }, 1000);
+  // }
+
+  // addClass3() {
+  // let element = document.getElementById("developers");
+  // element.classList.add("pulse");
+  // setTimeout(function(){ 
+  //   element.classList.remove("pulse"); 
+  // }, 1000);
+  // }
+  
+  // addClass4() {
+  // let element = document.getElementById("how-it-works");
+  // element.classList.add("pulse");
+  // setTimeout(function(){ 
+  //   element.classList.remove("pulse"); 
+  // }, 1000);
+  // }
 
   mobileMenu() {
     this.mobile = !this.mobile;
