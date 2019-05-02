f32e7747f6d2fffa4bde90e9dab25190e5376e4e
camila <camidlserna@hotmail.com>
2019-05-02 17:06:10 -0300

    Adding animations on home header

--- src/app/sections/home/home.component.ts
+++ src/app/sections/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { LandingAnimations } from '../../animations/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: LandingAnimations.animations
 })
 export class HomeComponent implements OnInit {
 
