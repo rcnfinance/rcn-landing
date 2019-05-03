f140c1fd00ed92e23248a7c6862fa5df50d3b057
camila <camidlserna@hotmail.com>
2019-05-03 16:07:53 -0300

    Easter egg on footer

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -6,15 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
+  
+  counter = 0;
+  
   constructor() { }
+  
 
   addClass(net) {
-    let element = document.getElementById(net);
-    element.classList.add("bounce");
-    setTimeout(function(){ 
+    this.counter++
+    if (this.counter >= 5) {
+      let element = document.getElementById(net);
+      element.classList.add("bounce");
+      setTimeout(function(){ 
       element.classList.remove("bounce"); 
     }, 1000);
+    this.counter = 0;
+    }
+    
     }
 
   ngOnInit() {
